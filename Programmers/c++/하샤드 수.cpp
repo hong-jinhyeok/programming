32b3dcfd9f2dabf972338755d6976b072e9412a9
hong-jinhyeok <76503802+hong-jinhyeok@users.noreply.github.com>
2021-03-12 15:38:39 +0900

    programmers-하샤드 수-c++

---
+++ Programmers/c++/하샤드 수.cpp
@@ -0,0 +1,22 @@
+/*
+프로그래머스 하샤드 수
+각자리수를 더한 수를 구함
+원래 수를 구한 수로 나눔
+나머지가 0인지에 따라 반환값 설정
+*/
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(int x) {
+    bool answer = true;
+    int y=x;
+    int sum=0;
+    while(y>0){
+        sum+=y%10;
+        y/=10;
+    }
+    if(x%sum==0) return answer;
+    return false;
+}
