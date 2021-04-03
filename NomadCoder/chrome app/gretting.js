370749db7ce2170c1d9bf69d5c4c806bed220f35
hong-jinhyeok <ghdzhf@nate.com>
2021-04-03 23:39:17 +0900

    Revert "fetch"
    This reverts commit 4902c8e724cbd990d65e7fb216b0130cd31384e5, reversing
    changes made to 7707f217d3c3df2ac481458cc37a57df20572d49.

--- NomadCoder/chrome app/gretting.js
+++ NomadCoder/chrome app/gretting.js
@@ -5,35 +5,17 @@ const greeting = document.querySelector(".js-grettings");
 const USER_LS = "currentUser";
 const SHOWING_CN = "showing";
 
-function saveName(text){
-    localStorage.setItem(USER_LS, text);
-}
-
 function paintGreeting(text){
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
     greeting.innerText = `Hello ${text}`;
 }
 
-function handleSubmit(event){
-    event.preventDefault();
-    const currentValue = input.value;
-    paintGreeting(currentValue);
-    saveName(currentValue);
-}
-
-function askForName(){
-    form.classList.add(SHOWING_CN);
-    form.addEventListener("submit",handleSubmit);
-}
-
-
-
 function loadName(){
     const currentUser = localStorage.getItem(USER_LS);
+    console.log(currentUser);
     if(currentUser===null){
-        console.log(currentUser);
-        askForName();
+
     }else{
         paintGreeting(currentUser);
     }
@@ -42,6 +24,5 @@ function loadName(){
 function init() {
     loadName();
 }
-
 
 init();
