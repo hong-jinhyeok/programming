ba1bb71b3f58b89a265a5b5dbb4853435e382a91
hong-jinhyeok <ghdzhf@nate.com>
2021-04-08 21:20:31 +0900

    nomadcoder-nwitter-setting

---
+++ NomadCoder/nwitter/src/components/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import AppRouter from "./Router";
+
+function App() {
+  return <AppRouter />;
+
+}
+
+export default App;
---
+++ NomadCoder/nwitter/src/firebase.js
@@ -0,0 +1,7 @@
+import firebase from "firebase/app";
+
+
+const firebaseConfig = {
+};
+
+export default firebase.initializeApp(firebaseConfig);
