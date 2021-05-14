431c6cd6e235ab1fcde4ffd182b73f6da69fd0eb
hong-jinhyeok <76503802+hong-jinhyeok@users.noreply.github.com>
2021-05-14 16:11:15 +0900

    nomadcoder-hook-useInput

---
+++ NomadCoder/hook/hook_useInput.js
@@ -0,0 +1,41 @@
+/*
+hook useState사용.
+App->hook사용
+AppUgly->hook사용X
+*/
+
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    const {
+      target: { value }
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+  return { value, onChange };
+};
+
+const App = () => {
+  const maxLen = (value) => value.length <= 10;
+  const name = useInput("Mr.", maxLen);
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <input placeholder="Name" {...name} />
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
