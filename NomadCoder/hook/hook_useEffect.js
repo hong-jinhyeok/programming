16f1d8995b2db8a4b38be38fd11908d649ae0747
hong-jinhyeok <76503802+hong-jinhyeok@users.noreply.github.com>
2021-05-17 10:18:20 +0900

    nomadcoder-hook-useEffect

---
+++ NomadCoder/hook/hook_useEffect.js
@@ -0,0 +1,23 @@
+/*
+hook useEffect 사용
+인자의 값의 변화에 따라 함수 호출이 가능
+*/ 
+import react, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  const sayHello = () => console.log("hello");
+  const [number, setNumber] = useState(0);
+  const [aNumber, setAnumber] = useState(0);
+  useEffect(sayHello, [aNumber, number]);
+  return (
+    <div className="App">
+      <div>Hi</div>
+      <button onClick={() => setNumber(number + 1)}>{number}</button>
+      <button onClick={() => setAnumber(aNumber + 1)}>{aNumber}</button>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
