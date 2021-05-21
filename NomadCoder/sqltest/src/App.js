50921377c8c5b871150e3a0b60fdc54e40474196
hong-jinhyeok <76503802+hong-jinhyeok@users.noreply.github.com>
2021-05-21 20:35:24 +0900

    nomadcoder-mysql-AppUpdate

--- NomadCoder/sqltest/src/App.js
+++ NomadCoder/sqltest/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import './App.css';
 export default class App extends Component {
   state = {
+    testid : '',
     testbody : "",
     testwriter : "",
   }
@@ -36,12 +37,41 @@ export default class App extends Component {
     });
   };
 //////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////
+getId = () => {
+  fetch("http://localhost:3001/data", { 
+    method: "post", //통신방법
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(),
+  })
+    .then((res) => res.json())
+    .then((json) => {
+      console.log(json);
+      this.setState({
+        testid: json.content_id,
+        testbody: json.content,
+        testwriter : json.writer
+      });
+    });
+};
+////////////////////////////////////////////////////////////////
+
   render() {
     return (
       <div>
         <input onChange={this.handleChange} name ="testbody"/>
         <input onChange={this.handleChange} name ="testwriter"/>
         <button onClick = {this.submitId}>Submit</button>
+        <h1>{this.state.testbody}</h1>
+        <br></br>
+        <h1>데이터 가져오기</h1>
+        <h3>{this.state.testid}</h3>
+        <h3>{this.state.testwriter}</h3>
+        <h3>{this.state.testbody}</h3>
+        <button onClick={this.getId}>가져오기</button>
       </div>
     )
   }
