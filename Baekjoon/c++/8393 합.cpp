0f842a82e0bbb72cc6a2b5b91b1a1a38a86defd4
hong-jinhyeok <76503802+hong-jinhyeok@users.noreply.github.com>
2021-06-26 01:22:39 +0900

    baekjoon-8393-c++

---
+++ Baekjoon/c++/8393 합.cpp
@@ -0,0 +1,16 @@
+/*
+백준 8393 합
+반복문을 통해 덧셈 진행
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int sum=0;
+    for(int i=1; i<=n; i++) sum+=i;
+    cout << sum;
+    return 0;
+}
