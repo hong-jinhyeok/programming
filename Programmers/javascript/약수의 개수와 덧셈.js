c6def129760db20f8c129b01f1fc8ce3dbc266ae
hong-jinhyeok <76503802+hong-jinhyeok@users.noreply.github.com>
2021-07-03 14:06:16 +0900

    programmers-약수의 개수와 덧셈-js

---
+++ Programmers/javascript/약수의 개수와 덧셈.js
@@ -0,0 +1,25 @@
+/*
+프로그래머스 약수의 개수와 덧셈
+약수의 개수를 파악하는 함수를 만들어 사용.
+짝수개면 true 홀수면 false 반환.
+그에 따라 덧셈과 뺄셈을 진행.
+*/
+function check(n){
+    let count=0;
+    for(let i=1; i<=n; i++){
+        if(n%i==0) count++;
+    }
+    if(count%2==0) return true;
+    else return false;
+}
+
+function solution(left, right) {
+    var answer = 0;
+    for(let i=left; i<=right; i++){
+        if(check(i)) answer+=i;
+        else answer-=i;
+    }
+    return answer;
+    return answer;
+}
+
