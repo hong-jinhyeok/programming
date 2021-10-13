db408084e0fb58137d348e9bf16527505d9b3097
hong-jinhyeok <76503802+hong-jinhyeok@users.noreply.github.com>
2021-10-13 22:36:17 +0900

    programmers-약수의 합-java

---
+++ Programmers/java/약수의 합.java
@@ -0,0 +1,13 @@
+/*
+약수의 합
+반복문을 통해 나누어 떨어지는 수들을 구하고 다 더함
+*/
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i=1; i<=n; i++){
+            if(n%i==0) answer+=i;
+        }
+        return answer;
+    }
+}
