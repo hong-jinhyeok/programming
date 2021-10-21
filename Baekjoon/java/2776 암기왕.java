346ee3c7fcc55788faa4fc55ead225532b58f209
hong-jinhyeok <76503802+hong-jinhyeok@users.noreply.github.com>
2021-10-21 23:45:02 +0900

    baekjoon-2776(2차)-java

--- Baekjoon/java/2776 암기왕.java
+++ Baekjoon/java/2776 암기왕.java
@@ -1,33 +1,41 @@
 /*
 백준 2776 암기왕
 arraylist에 담고 그 안에 원소 값이 있는지 확인
+2번째 방법으로 바이너리 서치를 사용 - > 시간초과 실패..
 - 시간초과 이유분석해야함.
 */
 import java.util.*;
+import java.io.*;
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner scanner = new Scanner(System.in);
-		
-		int n=scanner.nextInt();
-        while(n>0){
-            int n1,n2;
-            ArrayList<Integer> v = new ArrayList<>();
-            n1=scanner.nextInt();
-            for(int i =0; i<n1; i++){
-                int a;
-                a=scanner.nextInt();
-                v.add(a);
-            }
-            n2=scanner.nextInt();
-            for(int i =0; i<n2; i++){
-                int a;
-                a=scanner.nextInt();
-                if(v.contains(a) == false) System.out.println("0");
-                else System.out.println("1");
-            }
-            n--;
-        }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int testCase = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < testCase; i++) {
+		    int N = Integer.parseInt(br.readLine());
+		    StringTokenizer st = new StringTokenizer(br.readLine());
+
+		    ArrayList<Integer> v = new ArrayList<>();
+		    for (int j = 0; j < N; j++) {
+			v.add(Integer.parseInt(st.nextToken()));
+		    }
+		    //Collections.sort(v);
+		    int M = Integer.parseInt(br.readLine());
+
+		    st = new StringTokenizer(br.readLine());
+
+		    for (int j = 0; j < M; j++) {
+			int a = Integer.parseInt(st.nextToken());
+			if(Collections.binarySearch(v,a)>0){
+			    bw.write("1\n");
+			}
+			else {
+			    bw.write("0\n");
+			}
+		    }
+		}
 	}
 }
